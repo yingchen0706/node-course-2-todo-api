cb8c3d4e7d752373262e8e3bd699f95e9e2bdf71
yingchen0706 <yingchen0706@gmail.com>
2017-07-11 16:05:52 +0800

    add test POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+var {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -6,6 +7,7 @@ var {User} = require('./models/user')
 var {Todo} = require('./models/todo')
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -20,12 +22,32 @@ app.post('/todos', (req, res)=> {
     });
 });
 
-// app.get('/todo', (req, res)=> {
+app.get('/todos', (req, res)=> {
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    }, (err)=> {
+        res.status(400).send(e);
+    });
+});
 
-// });
+app.get('/todos/:id', (req, res)=> {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        if (todo) {
+            return res.status(200).send({todo});
+        } else {
+            return res.status(404).send();
+        }
+    }).catch((e)=> {
+        res.status(400).send(e);
+    });
+});
 
-app.listen(3000, ()=> {
-    console.log('Started on port 3000');
+app.listen(port, ()=> {
+    console.log(`Started on port ${port}`);
 })
 
 
